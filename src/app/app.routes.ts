791203df0001eaa64e00a93ac70f7f789861f762
haerphi <philippe.haerens@bstorm.be>
2025-11-10 09:34:59 +0100

    [Add] First pages and base routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { MovieListingPage } from './features/movie/pages/movie-listing-page/movie-listing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MovieListingPage,
+  },
+  {
+    path: 'movie',
+    loadChildren: () => import('./features/movie/movie.routes').then((r) => r.routes),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then((r) => r.routes),
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./features/admin/admin.routes').then((r) => r.routes),
+  },
+];
---
+++ src/app/features/admin/users/pages/user-listing-page/user-listing-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-listing-page',
+  imports: [],
+  templateUrl: './user-listing-page.html',
+  styleUrl: './user-listing-page.scss',
+})
+export class UserListingPage {
+
+}
