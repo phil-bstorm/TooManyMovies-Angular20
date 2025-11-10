7fad0cd6855b8cd3b66ddafaa3567d0fb62512f0
haerphi <philippe.haerens@bstorm.be>
2025-11-10 10:36:13 +0100

    [Add] Admin routing

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'movie/create',
+    loadComponent: () =>
+      import('./movie/pages/movie-create-page/movie-create-page').then((c) => c.MovieCreatePage),
+  },
+  {
+    path: 'movie/:id/update',
+    loadComponent: () =>
+      import('./movie/pages/movie-update-page/movie-update-page').then((c) => c.MovieUpdatePage),
+  },
+  {
+    path: 'user',
+    loadComponent: () =>
+      import('./users/pages/user-listing-page/user-listing-page').then((c) => c.UserListingPage),
+  },
+];
